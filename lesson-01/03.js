bc0b31fa4d4ba482c011508425134db93bc539ae
NataliaKengil <241283nata@gmail.com>
2024-05-24 00:55:46 +0300

    change homeWork 03.js

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,13 +1,8 @@
 let number = 1;
-
-console.log(number);
-number++;
-++number;
-console.log(number);
-console.log(number);
---number;
-number--;
-console.log(number);
+console.log(number++);
+console.log(++number);
+console.log(number--);
+console.log(--number);
 
 
 
